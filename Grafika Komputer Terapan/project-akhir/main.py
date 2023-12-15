2f93f82ef08036d24680bfd49e8665af58d0489e
dafiraone <irawandaffa7@gmail.com>
2023-12-15 11:21:27 +0700

    update quiz

--- Grafika Komputer Terapan/project-akhir/main.py
+++ Grafika Komputer Terapan/project-akhir/main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import math
+import random
 
 # Initialize Pygame
 pygame.init()
@@ -18,6 +19,7 @@ RED = (255, 0, 0)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Project Akhir Grafika Komputer Terapan - Kelompok 9")
 clock = pygame.time.Clock()
+surface = pygame.Surface((WIDTH, HEIGHT))
 
 # Background image (replace 'background.jpg' with the path to your image)
 try:
@@ -31,7 +33,7 @@ font = pygame.font.Font(None, 36)
 header_font = pygame.font.Font(None, 48)
 
 # Menu options
-menu_options = ['Start', 'About', 'Exit']
+menu_options = ['Start', 'Cara Bermain', 'About', 'Exit']
 selected_menu = 0
 
 # Animation variables
@@ -61,57 +63,199 @@ current_rect_y = target_rect_y
 # Main loop
 running = True
 game_active = False
+cursor_position = []
+x = []
+y = []
+last_x = []
+last_y = []
+last_line = False
+draw_line = True
+on_reflection = True
+refleksi = random.choice("12345")
+jawaban = None
+
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                selected_menu = (selected_menu - 1) % len(menu_options)
-            elif event.key == pygame.K_DOWN:
-                selected_menu = (selected_menu + 1) % len(menu_options)
-            elif event.key == pygame.K_RETURN:
-                if menu_options[selected_menu] == "Start":
-                    game_active = True
-                elif menu_options[selected_menu] == "About":
-                    print("Grafika Komputer Terapan")
-                    print("Project Akhir - Start Menu pada Game")
-                    print('Kelompok 9')
-                    print('Muhammad Daffa Deli Junior Irawan - 152022003')
-                    print('Bagus Anugrah - 152022029\n')
-                elif menu_options[selected_menu] == "Exit":
-                    running = False
-
-                # Reset animation time when menu option is selected
-                # animation_time = 0
-
-            # Volume control with left and right arrow keys
-            elif event.key == pygame.K_LEFT:
-                if volume_step > 0:
-                    rotate_angle = (rotate_angle + 9) % 360
-                    volume_step = round(volume_step - 0.05, 2)
-                    volume = round(max(0, volume - 0.05), 2)
-                    pygame.mixer.music.set_volume(volume)
-            elif event.key == pygame.K_RIGHT:
-                if volume_step < 1:
-                    rotate_angle = (rotate_angle - 9) % 360
-                    volume_step = round(volume_step + 0.05, 2)
-                    volume = round(min(1, volume + 0.05), 2)
-                    pygame.mixer.music.set_volume(volume)
+        if game_active == False:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    selected_menu = (selected_menu - 1) % len(menu_options)
+                elif event.key == pygame.K_DOWN:
+                    selected_menu = (selected_menu + 1) % len(menu_options)
+                elif event.key == pygame.K_RETURN:
+                    if menu_options[selected_menu] == "Start":
+                        game_active = True
+                        continue
+                    elif menu_options[selected_menu] == "Cara Bermain":
+                        print("Tekan enter untuk memilih menu")
+                        print("Tekan panah atas/bawah untuk navigasi menu")
+                        print("Tekan panah kiri/kanan untuk kontrol volume")
+                        print("Klik kiri mouse untuk membuat garis")
+                        print("Ketik angka 1-5 untuk menjawab pertanyaan")
+                    elif menu_options[selected_menu] == "About":
+                        print("Grafika Komputer Terapan")
+                        print("Project Akhir - Start Menu pada Game")
+                        print('Kelompok 9')
+                        print('Muhammad Daffa Deli Junior Irawan - 152022003')
+                        print('Bagus Anugrah - 152022029\n')
+                    elif menu_options[selected_menu] == "Exit":
+                        running = False
+                elif event.key == pygame.K_LEFT:
+                    if volume_step > 0:
+                        rotate_angle = (rotate_angle + 9) % 360
+                        volume_step = round(volume_step - 0.05, 2)
+                        volume = round(max(0, volume - 0.05), 2)
+                        pygame.mixer.music.set_volume(volume)
+                elif event.key == pygame.K_RIGHT:
+                    if volume_step < 1:
+                        rotate_angle = (rotate_angle - 9) % 360
+                        volume_step = round(volume_step + 0.05, 2)
+                        volume = round(min(1, volume + 0.05), 2)
+                        pygame.mixer.music.set_volume(volume)
+        else:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(event.pos)
+                cursor_position.append(event.pos)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    draw_line = False
+                elif event.key == pygame.K_BACKSPACE:
+                    cursor_position = []
+                    x = []
+                    y = []
+                    last_x = []
+                    last_y = []
+                    last_line = False
+                    draw_line = True
+                    on_reflection = True
+                    game_active = False
+                elif event.key == pygame.K_1:
+                    jawaban = '1'
+                elif event.key == pygame.K_2:
+                    jawaban = '2'
+                elif event.key == pygame.K_3:
+                    jawaban = '3'
+                elif event.key == pygame.K_4:
+                    jawaban = '4'
+                elif event.key == pygame.K_5:
+                    jawaban = '5'
+                elif event.key == pygame.K_LEFT:
+                    if volume_step > 0:
+                        rotate_angle = (rotate_angle + 9) % 360
+                        volume_step = round(volume_step - 0.05, 2)
+                        volume = round(max(0, volume - 0.05), 2)
+                        pygame.mixer.music.set_volume(volume)
+                elif event.key == pygame.K_RIGHT:
+                    if volume_step < 1:
+                        rotate_angle = (rotate_angle - 9) % 360
+                        volume_step = round(volume_step + 0.05, 2)
+                        volume = round(min(1, volume + 0.05), 2)
+                        pygame.mixer.music.set_volume(volume)
 
     if game_active:
-        screen.fill(WHITE)  # Clear the screen
-        pygame.display.set_caption("Game Window")
+        screen.fill(BLACK)
+        if draw_line:
+            draw_text = pygame.font.Font(None, 36).render('Gambar garis bebas', True, WHITE)
+            draw_text_rect= draw_text.get_rect(center=(WIDTH // 2, 50))
+            surface.blit(draw_text, draw_text_rect)
+        else:
+            text = 'Pencerminan Terhadap :\n1. sumbu x      2. sumbu y      3. garis y=x\n4. garis y=-x     5. titik asal O (0,0)\n- Ketik angka untuk menjawab -'
+            draw_text = [pygame.font.Font(None, 36).render(line, True, RED) for line in text.split('\n')]
+            for i, render_line in enumerate(draw_text):
+                draw_text_rect = render_line.get_rect(center=(WIDTH // 2, HEIGHT - 200 + i * 40))
+                surface.blit(render_line, draw_text_rect)
+
+        screen.blit(surface, (0,0))
+
+        pygame.display.set_caption("Kuis Refleksi")
+        if draw_line and len(cursor_position) == 2:
+                x1 = cursor_position[0][0]
+                x2 = cursor_position[1][0]
+                y1 = cursor_position[0][1]
+                y2 = cursor_position[1][1]
+
+                dx = x2 - x1
+                dy = y2 - y1
+
+                if abs(dx) > abs(dy):
+                    r = abs(dx)
+                else:
+                    r = abs(dy)
+                    
+                try:
+                    xr = dx/r
+                    yr = dy/r
+                except:
+                    draw_line = False
+
+                i = 0
+
+                x.append(round(x1))
+                y.append(round(y1))
+                while i < r:
+                    x1 = x1 + xr
+                    y1 = y1 + yr
+                    x.append(round(x1))
+                    y.append(round(y1))
+                    i +=1
+                cursor_position.pop(0)
+
+        if draw_line == False:
+            for i in range(len(x)):
+                surface.set_at((x[i], y[i]), 'black')
+
+            if refleksi == '1':
+                for point in range(len(y)):
+                    y[point] = HEIGHT - y[point]
+            elif refleksi == '2':
+                for point in range(len(x)):
+                    x[point] = WIDTH - x[point]
+            elif refleksi == '3':
+                for point in range(len(x)):
+                    x, y = y, x
+            elif refleksi == '4':
+                if last_line == True:
+                    x = last_x.copy()
+                    y = last_y.copy()
+                    last_line = False
+                else:
+                    last_x = x.copy()
+                    last_y = y.copy()
+                    for point in range(len(x)):
+                        x[point], y[point] = HEIGHT - y[point], WIDTH - x[point]
+                    last_line = True
+            elif refleksi == '5':
+                if last_line == True:
+                    x = last_x.copy()
+                    y = last_y.copy()
+                    last_line = False
+                else:
+                    last_x = x.copy()
+                    last_y = y.copy()
+                    for point in range(len(x)):
+                        x[point], y[point] = WIDTH - x[point], HEIGHT - y[point] 
+                    last_line = True
+
+            pygame.time.delay(500)
+            
+        for i in range(len(x)):
+            surface.set_at((x[i], y[i]), 'white')
+        if jawaban != None:
+            if jawaban == refleksi:
+                print('Jawaban benar')
+            else:
+                print('Jawaban salah')
     else:
-        # Draw background
         if background_image:
             screen.blit(background_image, (0, 0))
         else:
             screen.fill(WHITE)
 
-        # Draw header "START MENU" with underline
-        header_text = header_font.render("START MENU", True, WHITE)
+        
+        header_text = header_font.render("KUIS REFLEKSI", True, WHITE)
         header_text_rect = header_text.get_rect(center=(WIDTH // 2, 200))
         pygame.font.Font.set_underline(header_font, True)
         screen.blit(header_text, header_text_rect)
@@ -140,7 +284,7 @@ while running:
 
             screen.blit(text, text_rect)
 
-        # Interpolate the position of the rectangle for a smooth transition
+        # Interpolasi posisi untuk transisi
         target_rect_y = HEIGHT // 2 + selected_menu * 50
         current_rect_y += (target_rect_y - current_rect_y) * 0.1
 
@@ -148,30 +292,19 @@ while running:
         # rotated_volume_icon = pygame.transform.rotate(volume_icon, rotate_angle)
         rotated_volume_icon = pygame.transform.rotozoom(volume_icon, rotate_angle, 1)
 
-        # Set maximum width and height for the volume icon
-        # max_size = 100
-        # if rotated_volume_icon.get_width() > max_size or rotated_volume_icon.get_height() > max_size:
-        #     rotated_volume_icon = pygame.transform.scale(rotated_volume_icon, (max_size, max_size))
-
-        # Position the volume icon at the bottom right
         volume_icon_rect = rotated_volume_icon.get_rect(center=(WIDTH - 70, HEIGHT - 70))
         screen.blit(rotated_volume_icon, volume_icon_rect)
 
-        # Draw volume level text
         volume_text = font.render(f"Volume: {int(volume * 100)}%", True, WHITE)
         volume_text_rect = volume_text.get_rect(center=(WIDTH // 2, HEIGHT - 100))
         screen.blit(volume_text, volume_text_rect)
 
-    # Update the display
     pygame.display.flip()
 
-    # Update animation time
     animation_time += 1 / FPS
 
-    # Cap the frame rate
     clock.tick(FPS)
 
-# Quit Pygame
 pygame.mixer.music.stop()
 pygame.quit()
 sys.exit()
